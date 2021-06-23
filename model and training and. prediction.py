213b1d2450e973a08f3a634b79c3bbd939a4f339
Habib Ullah Khan Jamee <43148756+HabibJamee@users.noreply.github.com>
2021-06-23 20:10:35 +0600

    Create model and training and. prediction.py

---
+++ model and training and. prediction.py
@@ -0,0 +1,76 @@
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu', input_shape = (150, 150, 3)), 
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics=['accuracy'])
+
+
+history = model.fit(train_images, train_labels, batch_size=20, epochs=2, validation_split = 0.2)
+
+history = model.fit(train_images, train_labels, batch_size=20, epochs=100, validation_split = 0.2)
+
+
+
+def plot_accuracy_loss(history):
+    """
+        Plot the accuracy and the loss during the training of the nn.
+    """
+    fig = plt.figure(figsize=(10,5))
+
+    # Plot accuracy
+    plt.subplot(221)
+    plt.plot(history.history['accuracy'],'bo--', label = "acc")
+    plt.plot(history.history['val_accuracy'], 'ro--', label = "val_acc")
+    plt.title("train_acc vs val_acc")
+    plt.ylabel("accuracy")
+    plt.xlabel("epochs")
+    plt.legend()
+
+    # Plot loss function
+    plt.subplot(222)
+    plt.plot(history.history['loss'],'bo--', label = "loss")
+    plt.plot(history.history['val_loss'], 'ro--', label = "val_loss")
+    plt.title("train_loss vs val_loss")
+    plt.ylabel("loss")
+    plt.xlabel("epochs")
+
+    plt.legend()
+    plt.show()
+
+plot_accuracy_loss(history)
+
+test_loss = model.evaluate(test_images, test_labels)
+
+predictions = model.predict(test_images)     # Vector of probabilities
+pred_labels = np.argmax(predictions, axis = 1) # We take the highest probability
+
+display_random_image(class_names, test_images, pred_labels)
+
+
+def print_mislabeled_images(class_names, test_images, test_labels, pred_labels):
+    
+    BOO = (test_labels == pred_labels)
+    mislabeled_indices = np.where(BOO == 0)
+    mislabeled_images = test_images[mislabeled_indices]
+    mislabeled_labels = pred_labels[mislabeled_indices]
+
+    title = "Some examples of mislabeled images by the classifier:"
+    display_examples(class_names,  mislabeled_images, mislabeled_labels)
+    
+    print_mislabeled_images(class_names, test_images, test_labels, pred_labels)
+
+    
+    CM = confusion_matrix(test_labels, pred_labels)
+ax = plt.axes()
+sn.heatmap(CM, annot=True, 
+           annot_kws={"size": 10}, 
+           xticklabels=class_names, 
+           yticklabels=class_names, ax = ax)
+ax.set_title('Confusion matrix')
+plt.show()
